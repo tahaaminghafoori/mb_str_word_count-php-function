2dda5a8e1c5ef4dbf32d862caf6360753a873e62
Taha Amin Ghafuri <82469904+tahaghafuri@users.noreply.github.com>
2022-03-10 16:43:23 +0330

    Create mb_str_word_count.php

---
+++ mb_str_word_count.php
@@ -0,0 +1,34 @@
+<?php
+/*
+این تابع توسط طاها امین غفوری برنامه نویسی شده است!
+هرگونه حذف نام صاحب اثر پیگرد قانونی و شرعا حرام است.
+https://github.com/tahaghafuri/mb_str_word_count-php-function/
+https://tagteach.ir/
+*/
+if(!function_exists("mb_str_word_count")){
+function mb_str_word_count($str='ERR',$f=0) {
+	if ($str === null OR $str == '' OR $str == 'ERR') {
+		return "Error, value can't be empty!";
+                exit;
+	} else {
+            $as = explode(" ", $str);
+            
+            switch ($f) {
+                case 0:
+                    $r = count($as);
+                    break;
+                case 1:
+                    $r = array_values($as);
+                    break;
+                case 2:
+                    $r = array_values($as);
+                    break;
+                default:
+                    $r = "Please Enter Correct Number!";
+                    break;
+            }
+            
+            return $r;
+	}
+}
+} ?>
