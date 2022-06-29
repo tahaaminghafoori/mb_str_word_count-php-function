e344ee7ad19c9abb70f4ca7a09d884ea246bd2f2
Taha Amin Ghafuri <82469904+tahaghafuri@users.noreply.github.com>
2022-06-29 15:46:59 +0430

    fixed error
    return error fixed

--- mb_str_word_count.php
+++ mb_str_word_count.php
@@ -6,27 +6,27 @@ https://github.com/tahaghafuri/mb_str_word_count-php-function/
 https://tagteach.ir/
 */
 if(!function_exists("mb_str_word_count")){
-function mb_str_word_count($str='ERR',$f=0) {
-	if (empty($str) || $str == 'ERR') {
-		return "Error, value can't be empty!";
-                exit;
-	} else {
-            $as = explode(" ", $str);
+	function mb_str_word_count($str='ERR',$f=0) {
+		if (empty($str) || $str == 'ERR') {
+			$r = "Error, value can't be empty!";
+		} else {
+		    $as = explode(" ", $str);
 
-            switch ($f) {
-                case 0:
-                    $r = count($as);
-                    break;
-                case 1:
-                case 2:
-                    $r = array_values($as);
-                    break;
-                default:
-                    $r = "The format can only contain 0, 1 and 2!";
-                    break;
-            }
+		    switch ($f) {
+			case 0:
+			    $r = count($as);
+			    break;
+			case 1:
+			case 2:
+			    $r = array_values($as);
+			    break;
+			default:
+			    $r = "The format can only contain 0, 1 and 2!";
+			    break;
+		    }
 
-            return $r;
+		}
+		
+		return $r;
 	}
-}
 } ?>
